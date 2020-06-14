0ff46d16f3fe32b95582671863cefb023ce1aa61
Dragon <1redyoung@gmail.com>
2020-06-14 00:17:41 -0400

    Add project files.

---
+++ TalentMenuNames/Patch.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+using PulsarPluginLoader.Patches;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using static PulsarPluginLoader.Patches.HarmonyHelpers;
+
+namespace TalentMenuNames
+{
+    [HarmonyPatch(typeof(PLTabMenu), "UpdateTDs")]
+    class TalentMenuNameFix
+    {
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            List<CodeInstruction> targetSequence = new List<CodeInstruction>()
+            {
+                //new CodeInstruction(OpCodes.Ldloc_3),
+                new CodeInstruction(OpCodes.Callvirt, AccessTools.Method(typeof(PLPlayer), "GetClassName")),
+                new CodeInstruction(OpCodes.Callvirt, AccessTools.Method(typeof(System.String), "ToUpper")),
+            };
+
+            List<CodeInstruction> injectedSequence = new List<CodeInstruction>()
+            {
+                //new CodeInstruction(OpCodes.Ldloc_S, 3),
+                new CodeInstruction(OpCodes.Ldc_I4_0),
+                new CodeInstruction(OpCodes.Callvirt, AccessTools.Method(typeof(PLPlayer), "GetPlayerName")),
+            };
+
+            return HarmonyHelpers.PatchBySequence(instructions, targetSequence, injectedSequence, PatchMode.REPLACE);
+        }
+    }
+}
---
+++ TalentMenuNames/Plugin.cs
@@ -0,0 +1,20 @@
+﻿using PulsarPluginLoader;
+
+namespace TalentMenuNames
+{
+    class Plugin : PulsarPlugin
+    {
+        public override string Version => "0.0.2";
+
+        public override string Author => "Dragon";
+
+        public override string ShortDescription => "Modifies Talent Menu to display Player Names instead of Player Class";
+
+        public override string Name => "TalentMenuNames";
+
+        public override string HarmonyIdentifier()
+        {
+            return "Dragon.TalentMenuNames";
+        }
+    }
+}
